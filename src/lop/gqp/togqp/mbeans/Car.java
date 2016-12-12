5e7a3ab03cb523b366d9ee67b6ac685fbb52381a
kaku <xiaogqp@163.com>
2016-12-12 10:15:03 +0800

    i will test git pull command

---
+++ src/lop/gqp/togqp/mbeans/Car.java
@@ -0,0 +1,18 @@
+package com.gqp.togqp.mbeans;
+
+
+public class Car implements CarMBean {
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "this is a firstcar";
+	}
+
+	public void setName(String name) {
+		
+               //do nothing here at this moment;
+	       //guo-qp
+
+	}
+
+}
